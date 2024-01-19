5222b3b2791f23ed3c67229f7d5b6e304ba11dab
River <riverliway@gmail.com>
2024-01-18 22:10:59 -0800

    fix: make hash deterministic

--- src/crypto/hash.ts
+++ src/crypto/hash.ts
@@ -1,14 +1,13 @@
 const forge = require('node-forge')
 forge.options.usePureJavaScript = true
 
-const hasher = forge.md.sha256.create()
-
 /**
  * Hashes an object using SHA-256
  * @param obj - Object to hash
  * @returns the hash of the object
  */
 export const hash = (obj: any): string => {
+  const hasher = forge.md.sha256.create()
   hasher.update(JSON.stringify(obj))
   return hasher.digest().toHex()
 }
